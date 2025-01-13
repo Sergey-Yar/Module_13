7c17ec4b4bb13f02b57278d2eda514d49a880107
Сергей <ssergant@mail.ru>
2025-01-14 00:41:37 +0300

    простейшие CRUD функции для взаимодействия с базой данных Продуктовая база

---
+++ crud_functions.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+def initiate_db():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    )
+    ''')
+
+def get_all_product():
+    connection = sqlite3.connect('not_telegram.db')
+    cursor = connection.cursor()
+    get_all = cursor.execute('SELECT * FROM Products')
+    products = get_all.fetchall()
+    connection.commit()
+    connection.close()
+    return products
+
+
+# get_all = cursor.execute('SELECT * FROM Products')
+# products = get_all.fetchall()
+# for i in products:
+#     print(i[1])
+
+
+
+connection.commit()
+connection.close()
+
