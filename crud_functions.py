c4bccc631fb2f3ea9eb1d329209c9091068f474d
Сергей <ssergant@mail.ru>
2025-01-26 21:04:47 +0300

    Взаимодействие с БД

--- crud_functions.py
+++ crud_functions.py
@@ -12,17 +12,29 @@ def initiate_db():
     price INTEGER NOT NULL
     )
     ''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    )
+    ''')
 
 def get_all_product():
-    connection = sqlite3.connect('not_telegram.db')
-    cursor = connection.cursor()
     get_all = cursor.execute('SELECT * FROM Products')
     products = get_all.fetchall()
-    connection.commit()
-    connection.close()
     return products
 
+def add_user(username, email, age):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)', (username, email, age, 1000))
+    connection.commit()
+
+def is_included(username):
+    chek_user = cursor.execute('SELECT * FROM Users WHERE username=?', (username,))
+    return chek_user.fetchone() is None
 
 connection.commit()
-connection.close()
+# connection.close()
 
